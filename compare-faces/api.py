1f022e02e879741ac9ac9dac0fc5996fcf398b85
Yossapol Witayanont <reef.yossapol@bitkub.com>
2021-12-14 23:26:02 +0700

    first commit

---
+++ compare-faces/api.py
@@ -0,0 +1,99 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import face_recognition
+from PIL import Image
+import base64
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+img_path = basedir + '/images'
+img_path_original = img_path + '/original/'
+img_path_idcard = img_path + '/idcard/'
+img_path_selfie = img_path + '/selfie/'
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'image processing service'
+
+
+def convert_img_to_base64(img_path):
+    with open(img_path, "rb") as img_file:
+        encoded_string = base64.b64encode(img_file.read())
+    return encoded_string
+
+
+def convert_base64_to_img(img_base64, img_path, extension):
+    with open(f'{img_path}.{extension}', 'wb') as f:
+        f.write(base64.decodebytes(str.encode(img_base64)))
+
+
+@app.route('/compare-images', methods=['POST'])
+def image_processing():
+
+    if not request.json['img1_base64'] or not request.json['img2_base64']:
+        return jsonify(status="error", message="PLEASE_UPLOAD_2_IMAGES_TO_CONTINUE")
+
+    img1_base64 = request.json['img1_base64']
+    img2_base64 = request.json['img2_base64']
+    img_extension = request.json['img_extension']
+    customer_name = request.json['customer_name']
+    tolerate = request.json['tolerate']
+
+    convert_base64_to_img(
+        img1_base64, f'{img_path_original}{customer_name}-1', img_extension)
+    convert_base64_to_img(
+        img2_base64, f'{img_path_original}{customer_name}-2', img_extension)
+
+    load_img1 = face_recognition.load_image_file(
+        f'{img_path_original}{customer_name}-1.{img_extension}')
+    load_img2 = face_recognition.load_image_file(
+        f'{img_path_original}{customer_name}-2.{img_extension}')
+
+    facelocation_img1 = face_recognition.face_locations(load_img1)
+    facelocation_img2 = face_recognition.face_locations(load_img2)
+
+    if len(facelocation_img1) > 1 or len(facelocation_img2) > 1:
+        return jsonify(status="error", message="DETECT_MORE_THAN_ONE_FACE_IN_A_PICTURE")
+
+    if len(facelocation_img1) < 1 or len(facelocation_img2) < 1:
+        return jsonify(status="error", message="NO_FACE_DETECTED")
+
+    top, right, bottom, left = facelocation_img1[0]
+    coordinate_img1 = load_img1[top:bottom, left:right]
+    top, right, bottom, left = facelocation_img2[0]
+    coordinate_img2 = load_img2[top:bottom, left:right]
+
+    pil_img1 = Image.fromarray(coordinate_img1)
+    pil_img2 = Image.fromarray(coordinate_img2)
+
+    pil_img1.save(f'{img_path_idcard}{customer_name}-1.{img_extension}')
+    pil_img2.save(f'{img_path_selfie}{customer_name}-2.{img_extension}')
+
+    loadcrop_img1 = face_recognition.load_image_file(
+        f'{img_path_idcard}{customer_name}-1.{img_extension}')
+    loadcrop_img2 = face_recognition.load_image_file(
+        f'{img_path_selfie}{customer_name}-2.{img_extension}')
+
+    encoded_img1 = face_recognition.face_encodings(loadcrop_img1)
+    encoded_img2 = face_recognition.face_encodings(loadcrop_img2)
+
+    if not encoded_img1 or not encoded_img2:
+        return jsonify(status="error", message="CANNOT_ENCODE_IMAGE")
+
+    idcard_img = convert_img_to_base64(
+        f'{img_path_idcard}{customer_name}-1.{img_extension}')
+    selfie_img = convert_img_to_base64(
+        f'{img_path_selfie}{customer_name}-2.{img_extension}')
+
+    ds = face_recognition.face_distance([encoded_img1[0]], encoded_img2[0])[0]
+    isp = ds < tolerate
+
+    return jsonify(status="success", idcard_img=idcard_img.decode('utf-8'), selfie_img=selfie_img.decode('utf-8'), is_same_person=str(isp).lower(), diff_score=ds)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ identify-persons/identify_persons.py
@@ -0,0 +1,59 @@
+from PIL import Image, ImageDraw, ImageFont
+import face_recognition
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+face_list, name_list = [], []
+
+for i in os.listdir('./images'):
+    if '.DS_Store' not in i:
+        image = face_recognition.load_image_file(f'{basedir}/images/{i}')
+        image_encoding = face_recognition.face_encodings(image)[0]
+        face_list.append(image_encoding)
+        name_list.append(os.path.splitext(i)[0])
+
+# print(face_list)
+# print(name_list)
+
+target_img = face_recognition.load_image_file(f'{basedir}/target.jpg')
+face_locations = face_recognition.face_locations(target_img)
+face_encodings = face_recognition.face_encodings(target_img, face_locations)
+
+pil_img = Image.fromarray(target_img)
+draw = ImageDraw.Draw(pil_img)
+
+bk_green = (2, 215, 103)
+font = font = ImageFont.truetype("./fonts/Verdana.ttf", 28)
+
+tolerate = 0.5
+
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    matches = face_recognition.compare_faces(
+        face_list, face_encoding, tolerate)
+    name = 'unrecognized'
+
+    if True in matches:
+        match_index = matches.index(True)
+        name = f'{name_list[match_index]}'
+
+    draw.rectangle(((0, 0), (250, 50)), fill=bk_green, outline=bk_green)
+    draw.text((10, 10), f'tolerate = {tolerate}', fill=(0, 0, 0), font=font)
+
+    draw.rectangle(((left, top), (right, bottom)),
+                   outline=bk_green, width=5)
+    txt_width, txt_height = draw.textsize(name)
+
+    if (right - left) / 2 < txt_width + 5:
+        draw.rectangle(((left, bottom - txt_height), (right + txt_width / 1.5,
+                                                      bottom + txt_height + 15)), fill=bk_green, outline=bk_green)
+    else:
+        draw.rectangle(((left, bottom - txt_height), (right, bottom +
+                                                      txt_height + 15)), fill=bk_green, outline=bk_green)
+
+    draw.text((left + 5, bottom - txt_height),
+              name, fill=(0, 0, 0), font=font)
+
+del draw
+
+pil_img.show()
